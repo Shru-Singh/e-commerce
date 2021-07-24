d50ad73a8e1dd4a2f83e25f9a19d1e8d4bcec638
Shru-Singh <gshrutisingh@gmail.com>
2021-07-24 18:59:13 +0530

    delete reqeust added

--- router/product.js
+++ router/product.js
@@ -18,10 +18,27 @@ router.post('/products', async(req, res) => {
     try {
         const {product} = req.body;
         const products= await Product.create ({product})
-     return res.status(201).json(products)
+        const produce= await Product.find()
+     return res.status(200).json(produce)
     }catch (error){
         return res.status(500).json({"error": error})
     }
+})
+
+router.delete('/products/:id', async (req, res)=> {
+     try {
+         const _id=req.params.id ;
+         const products= await Product.deleteOne({_id})
+         console.log(req.params)
+         if(products.deletecount === 0) {
+         return res.status(404).json({})
+         }else{
+              const produce = await Product.find()
+             return res.status(200).json(produce)
+         }
+     }catch (error) {
+         return res.status(500).json({"error": error})
+     }
 })
 
 module.exports = router;
